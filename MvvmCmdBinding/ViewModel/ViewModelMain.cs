0eb15735307894aa6167fda8ec39154d7b2f988e
ff_wang <1105936347@qq.com>
2021-08-08 22:19:19 +0800

    :art: 完成 MVVM 案例

--- MvvmCmdBinding/ViewModel/ViewModelMain.cs
+++ MvvmCmdBinding/ViewModel/ViewModelMain.cs
@@ -2,11 +2,7 @@
 using GalaSoft.MvvmLight.CommandWpf;
 using MvvmCmdBinding.Model;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
