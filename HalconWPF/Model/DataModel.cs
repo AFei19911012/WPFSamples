56389da304c7d3c3528aca226d43af749d4464d3
ff_wang <1105936347@qq.com>
2021-08-18 23:56:20 +0800

    :construction: working on it

---
+++ HalconWPF/Model/DataModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace HalconWPF.Model
+{
+    ///
+    /// ----------------------------------------------------------------
+    /// Copyright @Taosy.W 2021 All rights reserved
+    /// Author      : Taosy.W
+    /// Created Time: 2021/8/18 23:36:08
+    /// Description :
+    /// ------------------------------------------------------
+    /// Version      Modified Time         Modified By    Modified Content
+    /// V1.0.0.0     2021/8/18 23:36:08    Taosy.W                 
+    ///
+    public class DataModel : ViewModelBase
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+
+        public string ImgPath { get; set; }
+    }
+}
---
+++ HalconWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using GalaSoft.MvvmLight;
+using HalconWPF.Model;
+using System.Collections.ObjectModel;
+
+namespace HalconWPF.ViewModel
+{
+    ///
+    /// ----------------------------------------------------------------
+    /// Copyright @Taosy.W 2021 All rights reserved
+    /// Author      : Taosy.W
+    /// Created Time: 2021/8/18 23:30:04
+    /// Description :
+    /// ------------------------------------------------------
+    /// Version      Modified Time         Modified By    Modified Content
+    /// V1.0.0.0     2021/8/18 23:30:04    Taosy.W                 
+    ///
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int selectedIndex;
+        public int SelectedIndex
+        {
+            get => selectedIndex;
+            set => Set(ref selectedIndex, value);
+        }
+
+        private ObservableCollection<DataModel> dataList;
+        public ObservableCollection<DataModel> DataList
+        {
+            get => dataList;
+            set => Set(ref dataList, value);
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            SelectedIndex = -1;
+            DataList = GetDataList();
+        }
+
+        private ObservableCollection<DataModel> GetDataList()
+        {
+            return new ObservableCollection<DataModel>
+            {
+                new DataModel{ Name = "AcquisitionImage"},
+            };
+        }
+    }
+}
